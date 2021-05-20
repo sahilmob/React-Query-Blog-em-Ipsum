df060e31a1da005aadc6dc7cab4ca0274cea3643
Sahil Mobaidin <sahil.hmob@hotmail.com>
2021-05-20 03:45:38 +0300

    set stale time

--- src/Posts.jsx
+++ src/Posts.jsx
@@ -16,7 +16,9 @@ export function Posts() {
   const [selectedPost, setSelectedPost] = useState(null);
 
   // replace with useQuery
-  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts);
+  const { data, isLoading, isError, error } = useQuery("posts", fetchPosts, {
+    staleTime: 2000,
+  });
 
   if (isLoading) return <div>Loading...</div>;
 
